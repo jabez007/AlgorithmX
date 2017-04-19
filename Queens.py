70fcbef014ba0c2a4e6d10fcf7ca4133562dec63
jabez007 <jabez.m.007@gmail.com>
2017-04-19 09:36:12 -0500

    Getting the Queens to work

--- Queens.py
+++ Queens.py
@@ -17,7 +17,6 @@ def solve_queens(grid, Q=4):
          [("c", c) for c in range(N)] +  # each column in the grid
          [("rd", rd) for rd in right_diagonal_set(N)] +  # each right diagonal in the grid
          [("ld", ld) for ld in left_diagonal_set(N)])  # each left diagonal in the grid
-    print("X", X)
 
     Y = dict()
     '''
@@ -29,33 +28,32 @@ def solve_queens(grid, Q=4):
             - left_diagonal
     '''
     for r, c in product(range(N), range(N)):
-        rc_subset = [("r", r), ("c", c)]
-
         rd_moves = right_diagonal((r, c), N)
-        if rd_moves:
-            rc_subset.append(("rd", rd_moves[0]))
-
         ld_moves = left_diagonal((r, c), N)
-        if ld_moves:
-            rc_subset.append(("ld", ld_moves[0]))
 
-        Y[(r, c, "Q")] = rc_subset
-    print("Y", Y)
+        if rd_moves and ld_moves:
+            rd = ("rd", rd_moves[0])
+            ld = ("ld", ld_moves[0])
+
+            if rd in X and ld in X:
+                Y[(r, c, "Q")] = [("r", r), ("c", c),
+                                  ("rd", rd_moves[0]),
+                                  ("ld", ld_moves[0])]
 
     X, Y = exact_cover(X, Y)
-    print("X", X)
-    print("Y", Y)
 
     '''Select the known elements of the puzzle grid'''
     for i, row in enumerate(grid):
-        for j, q in enumerate(row):
-            if q == "Q":
-                select(X, Y, (i, j, q))
+        for j, p in enumerate(row):
+            if p != "":
+                select(X, Y, (i, j, p))
 
     for solution in solve(X, Y, []):
-        print(solution)
-        for (r, c, q) in solution:
-            grid[r][c] = q
+        for r, c in product(range(N), range(N)):
+            grid[r][c] = ""
+            for p in ("Q", "B", "R", "K"):
+                if (r, c, p) in solution:
+                    grid[r][c] = p
         yield grid
 
 
@@ -123,7 +121,7 @@ def right_diagonal_set(N):
     rd = set()
     for rc in product(range(N), range(N)):
         rd_moves = right_diagonal(rc, N)
-        if rd_moves:
+        if rd_moves and left_diagonal(rd_moves[0], N):  # we have a right and left diagonal
             rd.add(rd_moves[0])
     return list(rd)
 
@@ -132,14 +130,14 @@ def left_diagonal_set(N):
     ld = set()
     for rc in product(range(N), range(N)):
         ld_moves = left_diagonal(rc, N)
-        if ld_moves:
+        if ld_moves and right_diagonal(ld_moves[0], N):  # we have a left and right diagonal
             ld.add(ld_moves[0])
     return list(ld)
 
 
 if __name__ == "__main__":
     puzzle = [
-        ["", "Q", "", ""],
+        ["", "", "", ""],
         ["", "",  "", ""],
         ["", "",  "", ""],
         ["", "",  "", ""]]

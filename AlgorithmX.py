e0beff82e9204837b5c239ffbf83cffe864f97c4
jabez007 <jabez.m.007@gmail.com>
2017-05-05 11:16:29 -0500

    Attempting to generalize with secondary columns

--- AlgorithmX.py
+++ AlgorithmX.py
@@ -4,8 +4,17 @@ from
 http://www.cs.mcgill.ca/~aassaf9/python/algorithm_x.html
 """
 
+SECONDARY = "secondary"
+
 
 def exact_cover(x, y):
+    """
+    converts a list/set of elements X into and dict where the keys are the set's elements and the values are the set of
+    subsets from Y that contain the element 
+    :param x: <list or set> all the elements that make up our universal set
+    :param y: <dict> the subsets that our set elements are divided in to
+    :return: <tuple(dict, dict)> X converted into a dict and Y unchanged
+    """
     x = {j: set() for j in x}
     for i, row in y.items():
         for j in row:
@@ -14,13 +23,15 @@ def exact_cover(x, y):
 
 
 def solve(x, y, solution=[]):
-    if not x:
+    if not x or \
+            (all(isinstance(k, tuple) for k in x.keys()) and all(k[-1] == SECONDARY for k in x.keys())):
         yield list(solution)
     else:
-        c = min(x, key=lambda c: len(x[c]))
-        for r in list(x[c]):
-            solution.append(r)
-            cols = select(x, y, r)
+        # find the element contained in the least number of subsets
+        c = min(x, key=lambda c: len(x[c]) if not isinstance(c, tuple) or c[-1] != SECONDARY else len(y.keys()))
+        for r in list(x[c]):  # for each subset that contains this element
+            solution.append(r)  # add that subset to our potential solution
+            cols = select(x, y, r)  # remove the columns for the other elements contained in this subset
             for s in solve(x, y, solution):
                 yield s
             deselect(x, y, r, cols)
@@ -29,12 +40,12 @@ def solve(x, y, solution=[]):
 
 def select(x, y, r):
     cols = []
-    for j in y[r]:
-        for i in x[j]:
-            for k in y[i]:
+    for j in y[r]:  # for each element in this subset
+        for i in x[j]:  # for each other subset that contains the element
+            for k in y[i]:  # for each other element in that subset
                 if k != j:
-                    x[k].remove(i)
-        cols.append(x.pop(j))
+                    x[k].remove(i)  # remove this subset from the other element; remove the row from the matrix
+        cols.append(x.pop(j))  # remove the column for the element; remove the column from the matrix
     return cols
 
 
@@ -47,9 +58,9 @@ def deselect(x, y, r, cols):
                     x[k].add(i)
 
 if __name__ == "__main__":
-    X = {1, 2, 3, 4, 5, 6, 7}
+    X = {1, 2, 3, 4, 5, 6, 7, (8, SECONDARY)}
     Y = {
-        'A': [1, 4, 7],
+        'A': [1, 4, 7, (8, SECONDARY)],
         'B': [1, 4],
         'C': [4, 5, 7],
         'D': [3, 5, 6],
